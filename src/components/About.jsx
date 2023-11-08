6c2e49d6111f47b2a5083022bc2780f9d1b7c3dd
Danish.ali <danish.ali@axiomworld.net>
2023-11-08 08:22:31 +0500

    [add] about section complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { styles } from "../style";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "./hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -49,4 +50,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
